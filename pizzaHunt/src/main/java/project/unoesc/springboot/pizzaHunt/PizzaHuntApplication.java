a961516ad2c8bafbbab10884dc1eee9de6082a76
Geslon Gisch <GeslonG@Outlook.com>
2023-11-26 17:34:44 -0300

    Criando javadoc

--- pizzaHunt/src/main/java/project/unoesc/springboot/pizzaHunt/PizzaHuntApplication.java
+++ pizzaHunt/src/main/java/project/unoesc/springboot/pizzaHunt/PizzaHuntApplication.java
@@ -1,3 +1,9 @@
+/**
+ * Classe principal responsável por iniciar a aplicação PizzaHunt.
+ * Esta classe utiliza o framework Spring Boot para iniciar a aplicação web.
+ * Ela é anotada com @SpringBootApplication para habilitar a configuração automática do Spring Boot
+ * e @EntityScan para especificar o pacote onde as entidades do modelo estão localizadas.
+ */
 package project.unoesc.springboot.pizzaHunt;
 
 import org.springframework.boot.SpringApplication;
@@ -8,8 +14,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 public class PizzaHuntApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PizzaHuntApplication.class, args);
-	}
+    /**
+     * Método principal (main) da aplicação.
+     * Inicializa a aplicação Spring Boot.
+     *
+     * @param args Argumentos passados para a aplicação via linha de comando.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaHuntApplication.class, args);
+    }
 
 }
